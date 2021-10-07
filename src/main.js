6a6a2ad4963492a91b9302f8c0356a7b53db2de3
Raymund Ács <raycsucsu@gmail.com>
2021-10-07 13:10:03 +0200

    Branch-coverage (#12)
    * Branch-coverage
    
    * Standardize branch coverage for HTML reports
    
    Co-authored-by: Zennon Gosalvez <1798166+zgosalvez@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -57,7 +57,7 @@ async function genhtml(coverageFiles, tmpPath) {
   const workingDirectory = core.getInput('working-directory').trim() || './';
   const artifactName = core.getInput('artifact-name').trim();
   const artifactPath = path.resolve(tmpPath, 'html').trim();
-  const args = [...coverageFiles];
+  const args = [...coverageFiles, '--rc', 'lcov_branch_coverage=1'];
 
   args.push('--output-directory');
   args.push(artifactPath);
