942362291ed62c3da082a6a10bb832713da3215b
Pavel Saman <psaman@cisco.com>
2023-08-24 10:30:07 +0200

    add: install `lcov` in pre script
    This action should install everything it will later need, not rely on other steps of jobs.

---
+++ src/pre.js
@@ -0,0 +1,16 @@
+const core = require('@actions/core');
+
+async function run() {
+  try {
+    console.log('Installing lcov');
+
+    const { execSync } = require('child_process');
+    execSync('sudo apt-get install --assume-yes lcov');
+
+    console.log('lcov installed successfully');
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
