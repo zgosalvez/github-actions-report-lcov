3d06f39a23cbf3e213a27d41cbda3a6a9923faf1
Paul Swartz <paul@paulswartz.net>
2024-10-31 10:02:55 -0400

    feat: support running under `workflow_run`

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const lcovTotal = require('lcov-total')
 const os = require('os')
 const path = require('path')
 
-const events = ['pull_request', 'pull_request_target']
+const detailPath = /^(?<path>.+?)\s*[|].+[|].+[|].+$/
 
 async function run () {
   try {
@@ -30,44 +30,49 @@ async function run () {
     const isMinimumCoverageReached = totalCoverage >= minimumCoverage
 
     const hasGithubToken = gitHubToken !== ''
-    const isPR = events.includes(github.context.eventName)
 
-    if (hasGithubToken && isPR) {
+    if (hasGithubToken) {
       const octokit = await github.getOctokit(gitHubToken)
       const summary = await summarize(coverageFile)
-      const details = await detail(coverageFile, workingDirectory, octokit)
-      const sha = github.context.payload.pull_request.head.sha
-      const shaShort = sha.substr(0, 7)
-      const commentHeaderPrefix = `### ${
-        titlePrefix ? `${titlePrefix} ` : ''
-      }[LCOV](https://github.com/marketplace/actions/report-lcov) of commit`
-      let body = `${commentHeaderPrefix} [<code>${shaShort}</code>](${
-        github.context.payload.pull_request.number
-      }/commits/${sha}) during [${github.context.workflow} #${
-        github.context.runNumber
-      }](../actions/runs/${
-        github.context.runId
-      })\n<pre>${summary}\n\nFiles changed coverage rate:${details}</pre>${
-        additionalMessage ? `\n${additionalMessage}` : ''
-      }`
+      for (const pr of pullRequests(github)) {
+        console.log(
+          `Calculating coverage for PR ${pr.number}, sha ${pr.head.sha}...`
+        )
+        const details = await detail(
+          coverageFile,
+          workingDirectory,
+          pr,
+          octokit
+        )
+        const shaShort = pr.head.sha.substr(0, 7)
+        const commentHeaderPrefix = `### ${
+          titlePrefix ? `${titlePrefix} ` : ''
+        }[LCOV](https://github.com/marketplace/actions/report-lcov) of commit`
+        let body = `${commentHeaderPrefix} [<code>${shaShort}</code>](${
+          pr.number
+        }/commits/${pr.head.sha}) during [${github.context.workflow} #${
+          github.context.runNumber
+        }](../actions/runs/${
+          github.context.runId
+        })\n<pre>${summary}\n\nFiles changed coverage rate:${details}</pre>${
+          additionalMessage ? `\n${additionalMessage}` : ''
+        }`
 
-      if (!isMinimumCoverageReached) {
-        body += `\n:no_entry: ${errorMessage}`
+        if (!isMinimumCoverageReached) {
+          body += `\n:no_entry: ${errorMessage}`
+        }
+
+        updateComment
+          ? await upsertComment(body, commentHeaderPrefix, pr, octokit)
+          : await createComment(body, pr, octokit)
       }
-
-      updateComment
-        ? await upsertComment(body, commentHeaderPrefix, octokit)
-        : await createComment(body, octokit)
-    } else if (!hasGithubToken) {
+    } else {
       core.info(
         'github-token received is empty. Skipping writing a comment in the PR.'
       )
       core.info(
         'Note: This could happen even if github-token was provided in workflow file. It could be because your github token does not have permissions for commenting in target repo.'
       )
-    } else if (!isPR) {
-      core.info('The event is not a pull request. Skipping writing a comment.')
-      core.info('The event type is: ' + github.context.eventName)
     }
 
     core.setOutput('total-coverage', totalCoverage)
@@ -76,26 +81,25 @@ async function run () {
       throw Error(errorMessage)
     }
   } catch (error) {
+    console.error(error)
     core.setFailed(error.message)
   }
 }
 
-async function createComment (body, octokit) {
+async function createComment (body, pullRequest, octokit) {
   core.debug('Creating a comment in the PR.')
 
   await octokit.rest.issues.createComment({
-    repo: github.context.repo.repo,
-    owner: github.context.repo.owner,
-    issue_number: github.context.payload.pull_request.number,
-    body
+    issue_number: pullRequest.number,
+    body,
+    ...ownerRepo(pullRequest.url)
   })
 }
 
-async function upsertComment (body, commentHeaderPrefix, octokit) {
+async function upsertComment (body, commentHeaderPrefix, pullRequest, octokit) {
   const issueComments = await octokit.rest.issues.listComments({
-    repo: github.context.repo.repo,
-    owner: github.context.repo.owner,
-    issue_number: github.context.payload.pull_request.number
+    issue_number: pullRequest.number,
+    ...ownerRepo(pullRequest.url)
   })
 
   const existingComment = issueComments.data.find((comment) =>
@@ -106,25 +110,63 @@ async function upsertComment (body, commentHeaderPrefix, octokit) {
     core.debug(`Updating comment, id: ${existingComment.id}.`)
 
     await octokit.rest.issues.updateComment({
-      repo: github.context.repo.repo,
-      owner: github.context.repo.owner,
       comment_id: existingComment.id,
-      body
+      body,
+      ...ownerRepo(pullRequest.url)
     })
   } else {
     core.debug('Comment does not exist, a new comment will be created.')
 
-    await createComment(body, octokit)
+    await createComment(body, pullRequest, octokit)
+  }
+}
+
+function pullRequests (github) {
+  if (github.context.eventName === 'pull_request') {
+    return [github.context.payload.pull_request]
+  }
+  if (github.context.eventName === 'workflow_run') {
+    if (github.context.payload.workflow_run.pull_requests.length > 0) {
+      return github.context.payload.workflow_run.pull_requests
+    }
+  }
+  if (
+    !!process.env.PR_SHA &&
+    !!process.env.PR_NUMBER &&
+    process.env.PR_SHA !== '' &&
+    process.env.PR_NUMBER !== ''
+  ) {
+    return [
+      {
+        number: process.env.PR_NUMBER,
+        head: {
+          sha: process.env.PR_SHA
+        },
+        url: `https://api.github.com/repos/${github.context.repo.owner}/${github.context.repo.repo}/pulls/${process.env.PR_NUMBER}`
+      }
+    ]
+  }
+  return []
+}
+
+function ownerRepo (url) {
+  const match = url.match(/\/repos\/(?<owner>[^/]+)\/(?<repo>[^/]+)\/pulls\//)
+  return {
+    owner: match[1],
+    repo: match[2]
   }
 }
 
 async function genhtml (coverageFiles, tmpPath, workingDirectory) {
   const artifactName = core.getInput('artifact-name').trim()
   const artifactPath = path.resolve(tmpPath, 'html').trim()
-  const args = [...coverageFiles, '--rc', 'lcov_branch_coverage=1']
-
-  args.push('--output-directory')
-  args.push(artifactPath)
+  const args = [
+    ...coverageFiles,
+    '--rc',
+    'lcov_branch_coverage=1',
+    '--artifact-directory',
+    artifactPath
+  ]
 
   await exec.exec('genhtml', args, { cwd: workingDirectory })
 
@@ -186,7 +228,7 @@ async function summarize (coverageFile) {
   return lines.join('\n')
 }
 
-async function detail (coverageFile, workingDirectory, octokit) {
+async function detail (coverageFile, workingDirectory, pullRequest, octokit) {
   let output = ''
 
   const options = {}
@@ -216,23 +258,27 @@ async function detail (coverageFile, workingDirectory, octokit) {
   lines.shift() // Removes "Reading tracefile..."
   lines.pop() // Removes "Total..."
   lines.pop() // Removes "========"
-
   const listFilesOptions = octokit.rest.pulls.listFiles.endpoint.merge({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    pull_number: github.context.payload.pull_request.number
+    pull_number: pullRequest.number,
+    ...ownerRepo(pullRequest.url)
   })
   const listFilesResponse = await octokit.paginate(listFilesOptions)
-  const changedFiles = listFilesResponse.map((file) => file.filename)
+  let changedFiles = listFilesResponse.map((file) => file.filename)
 
   lines = lines.filter((line, index) => {
     if (index <= 2) return true // Include header
 
-    for (const changedFile of changedFiles) {
-      console.log(`${line} === ${changedFile}`)
+    const match = detailPath.exec(line)
+    if (match) {
+      const linePath = path.resolve(workingDirectory, match.groups.path)
+      for (const changedFile of changedFiles) {
+        const relative = path.relative(linePath, changedFile)
+        console.log(`${linePath} === ${changedFile}`)
 
-      if (path.join(workingDirectory, line).startsWith(changedFile)) {
-        return true
+        if (relative === '') {
+          changedFiles = changedFiles.filter((x) => x !== changedFile)
+          return true
+        }
       }
     }
 
